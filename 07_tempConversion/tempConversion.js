5addf9a88aab71c4c5ac69d20dd437461340457a
lampizio <nikita030405@gmail.com>
2025-02-04 16:35:14 +0300

    feat: add logic to tempConversion func

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+  return Number(((degrees - 32) * 5/9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+  return Number((degrees * 9/5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
