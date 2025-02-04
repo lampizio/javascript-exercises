c2f0125a3fde35bcab737f2a45ef27d2c5f8ffa7
lampizio <nikita030405@gmail.com>
2025-02-04 16:14:12 +0300

    feat: add logic to sumAll func

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    if (
+        !(isInt(startNum) && isInt(endNum)) ||
+        !(isPositive(startNum) && isPositive(endNum))
+    ) {
+        return "ERROR";
+        }
 
+    if (startNum > endNum) {
+        let tempNum = startNum;
+        startNum = endNum;
+        endNum = tempNum;
+    }
+
+    let sum = 0;
+    for (let i = startNum; i <= endNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
+
+function isInt(number) {
+    return Number(number) === number && number % 1 === 0;
+}
+
+function isPositive(number) {
+    return number > 0;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
